712b28ed092682b418e64c3452821492161911ba
seu <douglastransformice01@outlook.com>
2026-07-09 23:27:23 -0300

    fade in/slide down

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ var main = function() {
     
     var $content = $(".contact").hide();
     var $moreContent = $(".sitemap").hide();
+    var $maintext = $(".main-text").hide();
+    var $maintext2 = $(".main-text2").hide();
+
 
     $("body").append($content);
 
@@ -11,6 +14,17 @@ var main = function() {
     $("body").append($moreContent);
 
     $moreContent.slideDown(2000);
+
+    $(".main-text").append($maintext);
+
+    $maintext.slideDown(2000, function() {
+        $(".main-text2").append($maintext2);
+
+        $maintext2.fadeIn(1000);
+    });
+
+
+
 };
 
 $(document).ready(main);
