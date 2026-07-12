c52178cc189ae190d2cffb1a6f49aa524b5ca2d5
seu <douglastransformice01@outlook.com>
2026-07-12 19:37:56 -0300

    add second tab

--- src/app_contend.js
+++ src/app_contend.js
@@ -1,27 +1,47 @@
 var main = function() {
     "use strict"
 
-    $(".tab a:nth-child(1)").on("click", function () {
-        $(".tab span").removeClass("active");
-        $(".tab a:nth-child(1) span").addClass("active");
-        $("main .content").empty();
-        return false;
-    });
+    var todos = [
+        "Finish writing book",
+        "Take Gracie to the park",
+        "Answer recruiter emails on LinkedIn",
+        "Prep for Monday's class",
+        "Make up some new ToDos",
+        "Get Groceries"
+    ]
 
-    $(".tab a:nth-child(2)").on("click", function () {
-        $(".tab span").removeClass("active");
-        $(".tab a:nth-child(2) span").addClass("active");
-        $("main .content").empty();
-        return false;
-    });
+    $(".tab span").toArray().forEach(function (element) {
+        $(element).on("click", function () {
 
-    $(".tab a:nth-child(3)").on("click", function () {
-        $(".tab span").removeClass("active");
-        $(".tab a:nth-child(3) span").addClass("active");
-        $("main .content").empty();
-        return false;
+            var $element = $(element), $content;
+                
+
+            $(".tab span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+
+            if($element.parent().is(":nth-child(1)")) {
+                console.log("FIRST TAB CLICKED!");
+                $content = $("<ul>");
+                for(let todo = todos.length -1; todo >= 0; todo--){
+                    $content.append($("<li>").text(todos[todo]));
+                }
+                $("main .content").append($content);
+            }else if ($element.parent().is(":nth-child(2)")) {
+                console.log("SECOND TAB CLICKED!");
+                $content = $("<ul>");
+                todos.forEach(function(todo) {
+                    $content.append($("<li>").text(todo));
+                });
+                $("main .content").append($content);
+            }else if ($element.parent().is(":nth-child(3)")) {
+                console.log("THIRD TAB CLICKED!");
+            }
+
+            return false;
     });
-    
-};
+ });
+//  $(".tab a:first-child span").trigger("click");
+}
 
 $(document).ready(main);
